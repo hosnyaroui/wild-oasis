4e9e9c4d4155b61b7988c851517b95b6dbb34f6c
hosnyaroui <hosny.cris@gmail.com>
2023-07-18 19:38:01 +0100

    Structuring our app and adding  Global Styles With Styled Components

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button  from './ui/Button';
+import Input  from './ui/Input';
+
+
+
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 600;
+background-color: yellow;
+`;
+
+
+
+const StyledApp = styled.div`
+background-color: orangered;
+padding: 20px;
+`;
+
+
+function App() {
+  
+
+  return (
+<>
+  <GlobalStyles />
+  <StyledApp>
+<H1>The wild Oasis</H1>
+<Button onClick={() => alert("check in")}>Check in</Button>
+<Button onClick={() => alert("check out")}>Check out</Button>
+<Input type='number' placeholder='Number of guests' />
+    </StyledApp>
+</>
+
+   
+    
+  )
+}
+
+export default App
