9df7b449f41b895b789954943b8daab1359b6d06
hosnyaroui <hosny.cris@gmail.com>
2023-07-19 21:27:03 +0100

    Styled compononet props and css function

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button  from './ui/Button';
 import Input  from './ui/Input';
+import Heading from './ui/Heading';
 
 
 
-const H1 = styled.h1`
-font-size: 30px;
-font-weight: 600;
-background-color: yellow;
-`;
+
 
 
 
@@ -26,9 +23,13 @@ function App() {
 <>
   <GlobalStyles />
   <StyledApp>
-<H1>The wild Oasis</H1>
+<Heading as='h1'>The wild Oasis</Heading>
+<Heading as='h2'>Check in and Out</Heading>
+
 <Button onClick={() => alert("check in")}>Check in</Button>
 <Button onClick={() => alert("check out")}>Check out</Button>
+
+<Heading as='h3'>Form</Heading>
 <Input type='number' placeholder='Number of guests' />
     </StyledApp>
 </>
