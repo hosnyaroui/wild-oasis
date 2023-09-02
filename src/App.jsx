f62bb23d0785cfdbd017c91ed171869a0c94c7ad
hosnyaroui <hosny.cris@gmail.com>
2023-09-02 14:13:26 +0100

    Setting up Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,33 @@
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GlobalStyles from './styles/GlobalStyles';
-import Button  from './ui/Button';
-import Input  from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import Dashboard from './pages/Dashboard';
+import Cabins from './pages/Cabins';
+import Bookings from './pages/Bookings';
+import Users from './pages/Settings';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
-
-
-
-
-
-
-const StyledApp = styled.div`
-
-padding: 20px;
-`;
-
-
-function App() {
-  
-
+const App = () => {
   return (
-<>
-  <GlobalStyles />
-  <StyledApp>
-  <Row>
-
-  
-
-
-  <Row type='horizontal'>
-<Heading as='h1'>The wild Oasis</Heading>
-<div>
-<Heading as='h2'>Check in and Out</Heading>
-
-<Button  onClick={() => alert("check in")}>Check in</Button>
-<Button onClick={() => alert("check out")}>Check out</Button>
-</div>
-</Row>
-
-<Row>
-<Heading as='h3'>Form</Heading>
-
-<form>
-<Input type='number' placeholder='Number of guests' />
-<Input type='number' placeholder='Number of guests' />
-</form>
-</Row>
-</Row>
-    </StyledApp>
-</>
-
-   
-    
+    <><GlobalStyles />
+    <BrowserRouter>
+      <Routes>
+      
+      <Route index element={<Navigate replace to="dashboard"/>} />
+<Route path="dashboard" element={<Dashboard />}/>
+<Route path="bookings" element={<Bookings />}/>
+<Route path="cabins" element={<Cabins />}/>
+<Route path="users" element={<Users />}/>
+<Route path="settings" element={<Settings />}/>
+<Route path="account" element={<Account />}/>
+<Route path="login" element={<Login />}/>
+<Route path="*" element={<PageNotFound />}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
-export default App
+export default App;
